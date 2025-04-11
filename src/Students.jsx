d8a45a8aa03dd33bbb2a4127e7c575f94808f7c3
Zack OConnor <zackary25@gmail.com>
2025-04-11 13:01:17 -0400

    Added a list of students

---
+++ src/Students.jsx
@@ -0,0 +1,15 @@
+const Students = () => {
+    return (
+        <>
+        <h1>Students</h1>
+        <ul>
+            <li>Alex</li>
+            <li>Ayah</li>
+            <li>Conor</li>
+            <li>Zack</li>
+        </ul>
+        </>
+    )
+}
+
+export default Students
