83871e2226faad8a9d3a65c65e902fd3fa98dc59
Conor Wantuch <cwantuch7@gmail.com>
2025-04-11 10:56:59 -0600

    added my discgolf object to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
+  const discGolfDiscs = [
+    { name: "Innova Destroyer", speed: 12, glide: 5, turn: -1, fade: 3 },
+    { name: "Discraft Nuke", speed: 13, glide: 5, turn: -1, fade: 3 },
+    { name: "Dynamic Discs Sheriff", speed: 13, glide: 5, turn: -1, fade: 2 },
+    { name: "Innova Leopard", speed: 6, glide: 5, turn: -2, fade: 1 },
+    { name: "Discmania FD", speed: 7, glide: 6, turn: -1, fade: 1 },
+    { name: "Latitude 64 River", speed: 7, glide: 7, turn: -1, fade: 1 },
+    { name: "Innova Roc", speed: 4, glide: 4, turn: 0, fade: 3 },
+    { name: "Discraft Buzzz", speed: 5, glide: 4, turn: -1, fade: 1 },
+    { name: "Dynamic Discs Judge", speed: 2, glide: 4, turn: 0, fade: 1 },
+    { name: "Innova Aviar", speed: 2, glide: 3, turn: 0, fade: 1 }
+  ];
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <h1>Hello world!</h1>
+  );
 }
 
 export default App
